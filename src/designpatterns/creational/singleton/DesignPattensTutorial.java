c9827b9eebb2865f8b3ef1a3c7d0984400875c95
Mert Çakar <mertcakar.es@gmail.com>
2020-02-20 23:53:51 +0300

    Project is created.

---
+++ src/designpatterns/creational/singleton/DesignPattensTutorial.java
@@ -0,0 +1,8 @@
+package designpatterns.creational.singleton;
+
+public class DesignPattensTutorial {
+
+	public static void main(String args[]) {
+		System.out.println("Application is started...");
+	}
+}
