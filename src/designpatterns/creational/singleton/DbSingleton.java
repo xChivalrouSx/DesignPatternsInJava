9737eca3e209d08f6f6b712ca33d2d77baba1246
Mert Çakar <mertcakar.es@gmail.com>
2020-02-22 00:01:50 +0300

    Lazy loading is added to singleton class. [DbSingleton]

--- src/designpatterns/creational/singleton/DbSingleton.java
+++ src/designpatterns/creational/singleton/DbSingleton.java
@@ -2,11 +2,15 @@ package designpatterns.creational.singleton;
 
 public class DbSingleton {
 
-	private static DbSingleton instance = new DbSingleton();
+	private static DbSingleton instance = null;
 	
 	private DbSingleton() {}
 	
 	public static DbSingleton getInstance() {
+		if(instance == null) {
+			instance = new DbSingleton();
+		}
+		
 		return instance;
 	}
 }
